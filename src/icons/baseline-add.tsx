f67cdf28511ff069358aeb4f4563142874b1cd92
intrnl <intrnl28+git@gmail.com>
2023-05-17 21:29:56 +0700

    fix: add missing divider on top circular

---
+++ src/icons/baseline-add.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from 'solid-js';
+
+const AddIcon = (props: ComponentProps<'svg'>) => {
+	return (
+		<svg width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+			<path fill='currentColor' d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z' />
+		</svg>
+	);
+};
+
+export default AddIcon;

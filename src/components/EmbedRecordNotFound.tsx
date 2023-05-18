0ae46b4d29ca31f99ea92ae2bc05490083bc6545
intrnl <intrnl28+git@gmail.com>
2023-05-18 20:59:20 +0700

    fix: alter not found wording

--- src/components/EmbedRecordNotFound.tsx
+++ src/components/EmbedRecordNotFound.tsx
@@ -1,7 +1,7 @@
 const EmbedRecordNotFound = () => {
 	return (
 		<div class='rounded-md border border-divider p-3'>
-			<p class='text-muted-fg'>This tweet is unavailable</p>
+			<p class='text-muted-fg text-sm'>This post is unavailable</p>
 		</div>
 	);
 };

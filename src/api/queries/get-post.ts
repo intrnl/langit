009baa7bc2b50a559d4d3a400e3e17fb3bd07923
intrnl <intrnl28+git@gmail.com>
2023-05-25 15:55:52 +0700

    feat: batched post retrieval

--- src/api/queries/get-post.ts
+++ src/api/queries/get-post.ts
@@ -3,27 +3,38 @@ import { type QueryFunctionContext } from '@tanstack/solid-query';
 import { mergeSignalizedPost } from '../cache/posts.ts';
 import { multiagent } from '../global.ts';
 import { type DID } from '../multiagent.ts';
-import { type BskyGetPostsResponse } from '../types.ts';
+import { type BskyGetPostsResponse, type BskyPost } from '../types.ts';
+
+import { createBatchedFetch } from '~/utils/batch-fetch.ts';
+
+type Query = ReturnType<typeof getPostKey>;
+
+const fetchPost = createBatchedFetch<Query, string, BskyPost>({
+	limit: 25,
+	timeout: 0,
+	key: (query) => query[1],
+	idFromQuery: (query) => query[2],
+	idFromData: (data) => data.uri,
+	fetch: async (queries) => {
+		const uid = queries[0][1];
+		const uris = queries.map((query) => query[2]);
+
+		const agent = await multiagent.connect(uid);
+
+		const response = await agent.rpc.get({
+			method: 'app.bsky.feed.getPosts',
+			params: { uris: uris },
+		});
+
+		const data = response.data as BskyGetPostsResponse;
+
+		return data.posts;
+	},
+});
 
 export const getPostKey = (uid: DID, uri: string) => ['getPost', uid, uri] as const;
 export const getPost = async (ctx: QueryFunctionContext<ReturnType<typeof getPostKey>>) => {
-	const [, uid, uri] = ctx.queryKey;
+	const post = await fetchPost(ctx.queryKey);
 
-	const agent = await multiagent.connect(uid);
-
-	const response = await agent.rpc.get({
-		method: 'app.bsky.feed.getPosts',
-		signal: ctx.signal,
-		params: { uris: [uri] },
-	});
-
-	const data = response.data as BskyGetPostsResponse;
-	const posts = data.posts;
-
-	if (posts.length > 0) {
-		const signalized = mergeSignalizedPost(posts[0]);
-		return signalized;
-	}
-
-	throw new Error(`Post not found`);
+	return mergeSignalizedPost(post);
 };

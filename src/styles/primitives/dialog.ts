425beb275d83d3201229f80d20ceefa80c87f967
intrnl <intrnl28+git@gmail.com>
2023-05-27 21:41:33 +0700

    refactor: remove no longer used dialog primitives

--- src/styles/primitives/dialog.ts
+++ src/styles/primitives/dialog.ts
@@ -1,11 +1,5 @@
 import { cva } from 'class-variance-authority';
 
-export const overlay = cva('fixed inset-0 z-20 bg-black bg-opacity-25');
-
-export const positioner = cva(
-	'fixed inset-0 z-20 flex min-h-full items-end justify-center overflow-y-auto sm:items-center sm:p-4',
-);
-
 export const content = cva(
 	'flex w-full max-w-md flex-col overflow-hidden rounded-t-md bg-background p-6 align-middle shadow-xl sm:rounded-md',
 );

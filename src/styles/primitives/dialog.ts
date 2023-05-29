2dc1fb7cc1d7ad8a4a6ab6227ed97bf70bd6efad
intrnl <intrnl28+git@gmail.com>
2023-05-29 14:20:30 +0700

    fix: prevent long titles from overflowing

--- src/styles/primitives/dialog.ts
+++ src/styles/primitives/dialog.ts
@@ -4,6 +4,6 @@ export const content = cva(
 	'flex w-full max-w-md flex-col overflow-hidden rounded-t-md bg-background p-4 pb-6 align-middle shadow-xl sm:rounded-md sm:pb-4',
 );
 
-export const title = cva('text-base font-bold');
+export const title = cva('text-base font-bold break-words');
 
 export const actions = cva('mt-6 flex justify-end gap-3');

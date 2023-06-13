c0a99ac47fab73843571fc599ef569a20d49afcf
intrnl <intrnl28+git@gmail.com>
2023-06-13 18:04:26 +0700

    feat: improved media viewer
    closes #3 #4

---
+++ src/icons/baseline-arrow-left.tsx
@@ -0,0 +1,11 @@
+import { type ComponentProps } from 'solid-js';
+
+const ArrowLeftIcon = (props: ComponentProps<'svg'>) => {
+	return (
+		<svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" />
+		</svg>
+	);
+};
+
+export default ArrowLeftIcon;

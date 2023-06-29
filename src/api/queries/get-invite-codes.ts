6c7004f1dcc5ff3bc8ec505cf0a4d8fc3ee543c8
intrnl <intrnl28+git@gmail.com>
2023-06-29 09:40:58 +0700

    fix: properly sort invite codes

--- src/api/queries/get-invite-codes.ts
+++ src/api/queries/get-invite-codes.ts
@@ -20,5 +20,19 @@ export const getInviteCodes: QueryFn<
 
 	const data = response.data as BskyGetInviteCodesResponse;
 
+	data.codes.sort((a, b) => {
+		const aUsed = a.uses.length >= a.available;
+		const bUsed = b.uses.length >= b.available;
+
+		if (aUsed === bUsed) {
+			const aDate = new Date(a.createdAt);
+			const bDate = new Date(b.createdAt);
+
+			return bDate.getTime() - aDate.getTime();
+		}
+
+		return aUsed ? 1 : -1;
+	});
+
 	return data;
 };

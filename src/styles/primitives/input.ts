b2cd2a00d23968d9693d00adb21c3b5ed12befa5
intrnl <intrnl28+git@gmail.com>
2023-07-24 12:47:57 +0700

    refactor: adjust input outlines

--- src/styles/primitives/input.ts
+++ src/styles/primitives/input.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 const input = cva(
-	'block h-9 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-fg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:opacity-50',
+	'block h-9 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-fg focus:outline-2 focus:-outline-offset-1 focus:outline-accent disabled:opacity-50',
 );
 
 export default input;

1d1b167ee148e04a6a77e3d576fe1f19fae85c04
intrnl <intrnl28+git@gmail.com>
2023-08-01 21:41:45 +0700

    fix: reintroduce ua matching on embeds

--- functions/r/_middleware.ts
+++ functions/r/_middleware.ts
@@ -6,6 +6,9 @@ const PROFILE_MATCHER = new URLPattern({ pathname: '/r/profile/:actor' });
 const POST_MATCHER = new URLPattern({ pathname: '/r/profile/:actor/post/:post' });
 const FEED_MATCHER = new URLPattern({ pathname: '/r/profile/:actor/feed/:feed' });
 
+const UA_RE =
+	/Discordbot|Slackbot|WhatsApp|TelegramBot|facebookexternalhit|Twitterbot|Viber|Linespider|MicroMessenger|WeChat|snapchat|SkypeUriPreview|Synapse|Googlebot|bingbot|YandexBot/;
+
 const appendHead = (response: Response, content: string) => {
 	const handler: HTMLRewriterElementContentHandlers = {
 		element: (element) => {
@@ -21,32 +24,36 @@ export const onRequest: PagesFunction = async (context) => {
 
 	const response = await context.next();
 
-	try {
-		const url = new URL(request.url);
+	const ua = request.headers.get('user-agent');
 
-		let match: URLPatternURLPatternResult | null;
-		let head: string | undefined;
+	if (ua && UA_RE.test(ua)) {
+		try {
+			const url = new URL(request.url);
 
-		if ((match = PROFILE_MATCHER.exec(url))) {
-			const { actor } = match.pathname.groups;
+			let match: URLPatternURLPatternResult | null;
+			let head: string | undefined;
 
-			head = await renderProfile(actor);
-		} else if ((match = POST_MATCHER.exec(url))) {
-			const { actor, post } = match.pathname.groups;
+			if ((match = PROFILE_MATCHER.exec(url))) {
+				const { actor } = match.pathname.groups;
 
-			head = await renderPost(actor, post);
-		} else if ((match = FEED_MATCHER.exec(url))) {
-			const { actor, feed } = match.pathname.groups;
+				head = await renderProfile(actor);
+			} else if ((match = POST_MATCHER.exec(url))) {
+				const { actor, post } = match.pathname.groups;
 
-			head = await renderFeed(actor, feed);
+				head = await renderPost(actor, post);
+			} else if ((match = FEED_MATCHER.exec(url))) {
+				const { actor, feed } = match.pathname.groups;
+
+				head = await renderFeed(actor, feed);
+			}
+
+			if (head) {
+				return appendHead(response, head);
+			}
+		} catch (err) {
+			console.log(`failed to render preview`);
+			console.error(err);
 		}
-
-		if (head) {
-			return appendHead(response, head);
-		}
-	} catch (err) {
-		console.log(`failed to render preview`);
-		console.error(err);
 	}
 
 	return response;

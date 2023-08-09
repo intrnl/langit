0debe9d0ea94ae9216c89624e47985bb9771df11
intrnl <intrnl28+git@gmail.com>
2023-08-09 12:19:53 +0700

    feat: put warning on safari users

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -5,6 +5,8 @@ import { A, useNavigate } from '~/router.ts';
 
 import button from '~/styles/primitives/button.ts';
 
+const isSafari = navigator.vendor.includes('Apple');
+
 const IndexPage = () => {
 	const navigate = useNavigate();
 
@@ -29,6 +31,33 @@ const IndexPage = () => {
 			<h1 class="text-lg font-bold">Langit</h1>
 
 			<p>Alternative Bluesky client</p>
+
+			{isSafari && (
+				<div class="mt-4 rounded-md bg-secondary p-4 text-sm text-secondary-fg">
+					<p class="font-bold">Incompatible browser/OS detected</p>
+					<p>Looks like you're using an iOS device, or Safari on a macOS device.</p>
+
+					<p class="mt-2">
+						Unfortunately, with Safari being unavailable on other platforms like Windows or Linux, and me not
+						having an Apple device I can test things on, there's just no way for me to support Langit running
+						on an iOS device or Safari on a macOS device.
+					</p>
+
+					<p class="mt-2">
+						Personally, I cannot justify paying for an expensive device where I'd only be using it to test
+						websites, and while I could always rely on your aid to help me fix any issues that may happen,
+						it's going to be hard to support Safari that way.
+					</p>
+
+					<p class="mt-2">
+						Instead, I'd be much happy if the Safari monopoly goes away, and you can visit the{' '}
+						<a href="https://open-web-advocacy.org" target="_blank" class="underline">
+							Open Web Advocacy website
+						</a>{' '}
+						for details on that matter.
+					</p>
+				</div>
+			)}
 
 			<div class="mt-8 flex gap-4">
 				<A href="/login" class={/* @once */ button({ color: 'primary' })}>

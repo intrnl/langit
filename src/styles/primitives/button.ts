1b86aff3cd7712ff5914c09c23a8e1e2ca22324f
intrnl <intrnl28+git@gmail.com>
2023-09-01 14:46:38 +0700

    fix: apply outline style consistently

--- src/styles/primitives/button.ts
+++ src/styles/primitives/button.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 const button = cva(
-	`inline-flex items-center rounded-md text-sm font-medium outline-2 focus-visible:outline disabled:pointer-events-none disabled:opacity-50`,
+	`inline-flex items-center rounded-md text-sm font-medium outline-2 outline-primary focus-visible:outline disabled:pointer-events-none disabled:opacity-50`,
 	{
 		variants: {
 			size: {
@@ -9,8 +9,8 @@ const button = cva(
 				sm: `h-9 px-4`,
 			},
 			color: {
-				primary: `bg-primary text-primary-fg hover:bg-primary/90 outline-offset-2 outline-primary`,
-				danger: `bg-red-600 text-primary hover:bg-red-700 outline-offset-2 outline-primary`,
+				primary: `bg-primary text-primary-fg hover:bg-primary/90 outline-offset-2`,
+				danger: `bg-red-600 text-primary hover:bg-red-700 outline-offset-2`,
 				secondary: `bg-secondary text-secondary-fg hover:bg-secondary/80`,
 				outline: `border border-input hover:bg-hinted hover:text-hinted-fg`,
 				ghost: `hover:bg-hinted hover:text-hinted-fg`,

fbacd4904d1ecaa659139c0b159a548ebc3e0497
intrnl <intrnl28+git@gmail.com>
2023-09-05 16:34:43 +0700

    refactor: proper outlining on menu items

--- src/styles/primitives/menu.ts
+++ src/styles/primitives/menu.ts
@@ -1,7 +1,7 @@
 import button from './button.ts';
 
 export const content = () => {
-	return 'flex w-full max-w-md max-h-[80%] flex-col overflow-hidden rounded-t-md bg-background pt-1 align-middle shadow-xl sm:rounded-md sm:pt-0';
+	return 'flex w-full max-w-md max-h-[80%] flex-col overflow-hidden rounded-t-md bg-background pt-1 align-middle shadow-xl sm:rounded-md';
 };
 
 export const title = () => {
@@ -9,7 +9,7 @@ export const title = () => {
 };
 
 export const item = () => {
-	return 'cursor-pointer flex items-center gap-4 px-4 py-3 text-left text-sm hover:bg-hinted disabled:pointer-events-none disabled:opacity-50';
+	return 'flex cursor-pointer items-center gap-4 px-4 py-3 text-left text-sm outline-2 -outline-offset-2 outline-primary hover:bg-hinted focus-visible:outline disabled:pointer-events-none disabled:opacity-50';
 };
 
 const cancelBase = button({ color: 'outline', class: 'mx-4 mt-3 mb-6 justify-center shrink-0 sm:mb-4' });

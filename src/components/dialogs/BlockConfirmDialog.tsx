705052bb58ddeb78c6816fbabf715a6879a152e2
intrnl <intrnl28+git@gmail.com>
2023-09-14 07:09:51 +0700

    refactor: inherit BlockConfirmDialog

--- src/components/dialogs/BlockConfirmDialog.tsx
+++ src/components/dialogs/BlockConfirmDialog.tsx
@@ -3,58 +3,37 @@ import type { DID } from '@intrnl/bluesky-client/atp-schema';
 import type { SignalizedProfile } from '~/api/cache/profiles.ts';
 import { blockProfile } from '~/api/mutations/block-profile.ts';
 
-import { closeModal } from '~/globals/modals.tsx';
+import ConfirmDialog from '~/components/dialogs/ConfirmDialog.tsx';
 
-import button from '~/styles/primitives/button.ts';
-import * as dialog from '~/styles/primitives/dialog.ts';
-
-export interface MuteConfirmDialogProps {
+export interface BlockConfirmDialogProps {
 	uid: DID;
 	profile: SignalizedProfile;
 }
 
-const BlockConfirmDialog = (props: MuteConfirmDialogProps) => {
+const BlockConfirmDialog = (props: BlockConfirmDialogProps) => {
 	const uid = () => props.uid;
 	const profile = () => props.profile;
 
 	const isBlocked = () => profile().viewer.blocking.value;
 
-	return (
-		<div class={/* @once */ dialog.content()}>
-			<h1 class={/* @once */ dialog.title()}>
-				{isBlocked() ? 'Unblock' : 'Block'} @{profile().handle.value}?
-			</h1>
-
-			{isBlocked() ? (
-				<p class="mt-3 text-sm">They will be allowed to view your posts, and interact with you.</p>
-			) : (
-				<p class="mt-3 text-sm">
-					They will not be able to view your posts, mention you, or otherwise interact with you, and you will
-					not see posts or replies from @{profile().handle.value}.
-				</p>
-			)}
-
-			<div class={/* @once */ dialog.actions()}>
-				<button
-					onClick={() => {
-						closeModal();
-					}}
-					class={/* @once */ button({ color: 'ghost' })}
-				>
-					Cancel
-				</button>
-				<button
-					onClick={() => {
-						blockProfile(uid(), profile());
-						closeModal();
-					}}
-					class={/* @once */ button({ color: 'primary' })}
-				>
-					{isBlocked() ? 'Unblock' : 'Block'}
-				</button>
-			</div>
-		</div>
-	);
+	return ConfirmDialog({
+		get title() {
+			return `${isBlocked() ? 'Unblock' : 'Block'} @${profile().handle.value}?`;
+		},
+		get body() {
+			return isBlocked()
+				? `They will be allowed to view your posts, and interact with you.`
+				: `They will not be able to view your posts, mention you, or otherwise interact with you, and you will not see posts or replies from @${
+						profile().handle.value
+				  }.`;
+		},
+		get confirmation() {
+			return isBlocked() ? `Unblock` : `Block`;
+		},
+		onConfirm: () => {
+			blockProfile(uid(), profile());
+		},
+	});
 };
 
 export default BlockConfirmDialog;

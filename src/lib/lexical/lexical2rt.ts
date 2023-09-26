56ed6c131692c42139f25070fdee1d3c867281ae
intrnl <intrnl28+git@gmail.com>
2023-09-27 06:40:48 +0700

    refactor: read nodes under a queue

--- src/lib/lexical/lexical2rt.ts
+++ src/lib/lexical/lexical2rt.ts
@@ -14,6 +14,8 @@ import { graphemeLen } from '~/api/richtext/intl.ts';
 import { toShortUrl } from '~/api/richtext/renderer.ts';
 import type { Facet } from '~/api/richtext/types.ts';
 
+import { Queue } from '~/utils/stack.ts';
+
 import type { MentionNode } from './MentionNode.ts';
 
 const encoder = new TextEncoder();
@@ -34,157 +36,164 @@ export const lexical2rt = (state: EditorState) => {
 	const facets: Facet[] = [];
 	const links: string[] = [];
 
-	const ignored = new WeakSet<LexicalNode>();
-
 	let text = '';
 	let length = 0;
 
 	let leading = true;
 	let ascii = true;
 
-	const delve = (node: LexicalNode) => {
-		if (ignored.has(node)) {
-			return;
-		}
+	const delve = (root: RootNode) => {
+		const ignored = new WeakSet<LexicalNode>();
+		const queue = new Queue<LexicalNode>();
 
-		const type = node.getType();
+		let node: LexicalNode | undefined;
 
-		if (type === 'root' || type === 'paragraph') {
-			const $node = node as RootNode | ParagraphNode;
-			const children = $node.getChildren();
+		queue.push(root);
 
-			for (let idx = 0, len = children.length; idx < len; idx++) {
-				const child = children[idx];
-				delve(child);
-			}
-		} else if (type === 'linebreak') {
-			if (!leading) {
-				text += '\n';
-				length += 1;
-			}
-		} else if (type === 'text') {
-			const $node = node as TextNode;
-			const $parent = $node.getParent<ParagraphNode>();
-
-			let value = $node.getTextContent();
-			let end = false;
-
-			if ($parent) {
-				const index = $node.getIndexWithinParent();
-
-				const children = $parent.getChildren();
-				const length = children.length;
-
-				end = index === length - 1 || (length > 1 && children[index + 1].getType() === 'linebreak');
+		while ((node = queue.shift())) {
+			if (ignored.has(node)) {
+				continue;
 			}
 
-			if (leading) {
-				value = end ? value.trim() : value.trimStart();
+			const type = node.getType();
+
+			if (type === 'root' || type === 'paragraph') {
+				const $node = node as RootNode | ParagraphNode;
+				const children = $node.getChildren();
+
+				for (let idx = 0, len = children.length; idx < len; idx++) {
+					const child = children[idx];
+					queue.push(child);
+				}
+			} else if (type === 'linebreak') {
+				if (!leading) {
+					text += '\n';
+					length += 1;
+				}
+			} else if (type === 'text') {
+				const $node = node as TextNode;
+				const $parent = $node.getParent<ParagraphNode>();
+
+				let value = $node.getTextContent();
+				let end = false;
+
+				if ($parent) {
+					const index = $node.getIndexWithinParent();
+
+					const children = $parent.getChildren();
+					const length = children.length;
+
+					end = index === length - 1 || (length > 1 && children[index + 1].getType() === 'linebreak');
+				}
+
+				if (leading) {
+					value = end ? value.trim() : value.trimStart();
+					leading = false;
+				} else if (end) {
+					value = value.trimEnd();
+				}
+
+				if (value.length === 0) {
+					continue;
+				}
+
+				text += value;
+
+				if (isAscii(value)) {
+					length += value.length;
+				} else {
+					length += encoder.encode(value).byteLength;
+					ascii = false;
+				}
+			} else if (type === 'mention') {
+				const $node = node as MentionNode;
+
+				const handle = $node.getTextContent();
+				const did = $node.__did;
+
+				const start = length;
+
+				text += handle;
+				length += handle.length;
+
 				leading = false;
-			} else if (end) {
-				value = value.trimEnd();
-			}
 
-			if (value.length === 0) {
-				return;
-			}
-
-			text += value;
-
-			if (isAscii(value)) {
-				length += value.length;
-			} else {
-				length += encoder.encode(value).byteLength;
-				ascii = false;
-			}
-		} else if (type === 'mention') {
-			const $node = node as MentionNode;
-
-			const handle = $node.getTextContent();
-			const did = $node.__did;
-
-			const start = length;
-
-			text += handle;
-			length += handle.length;
-
-			leading = false;
-
-			facets.push({
-				index: {
-					byteStart: start,
-					byteEnd: length,
-				},
-				features: [
-					{
-						$type: 'app.bsky.richtext.facet#mention',
-						did: did,
+				facets.push({
+					index: {
+						byteStart: start,
+						byteEnd: length,
 					},
-				],
-			});
-		} else if (type === 'hashtag') {
-			const $node = node as HashtagNode;
+					features: [
+						{
+							$type: 'app.bsky.richtext.facet#mention',
+							did: did,
+						},
+					],
+				});
+			} else if (type === 'hashtag') {
+				const $node = node as HashtagNode;
 
-			const content = $node.getTextContent();
+				const content = $node.getTextContent();
 
-			const start = length;
+				const start = length;
 
-			text += content;
+				text += content;
 
-			if (isAscii(content)) {
-				length += content.length;
-			} else {
-				length += encoder.encode(content).byteLength;
-				ascii = false;
-			}
+				if (isAscii(content)) {
+					length += content.length;
+				} else {
+					length += encoder.encode(content).byteLength;
+					ascii = false;
+				}
 
-			leading = false;
+				leading = false;
 
-			facets.push({
-				index: {
-					byteStart: start,
-					byteEnd: length,
-				},
-				features: [
-					{
-						$type: 'app.bsky.richtext.facet#tag',
-						tag: content.slice(1),
+				facets.push({
+					index: {
+						byteStart: start,
+						byteEnd: length,
 					},
-				],
-			});
-		} else if (type === 'autolink') {
-			const $node = node as AutoLinkNode;
+					features: [
+						{
+							$type: 'app.bsky.richtext.facet#tag',
+							tag: content.slice(1),
+						},
+					],
+				});
+			} else if (type === 'autolink') {
+				const $node = node as AutoLinkNode;
 
-			const url = $node.getURL();
-			const content = toShortUrl(url);
+				const url = $node.getURL();
+				const content = toShortUrl(url);
 
-			const start = length;
+				const start = length;
 
-			text += content;
+				text += content;
 
-			if (isAscii(content)) {
-				length += content.length;
-			} else {
-				length += encoder.encode(content).byteLength;
-				ascii = false;
-			}
+				if (isAscii(content)) {
+					length += content.length;
+				} else {
+					length += encoder.encode(content).byteLength;
+					ascii = false;
+				}
 
-			leading = false;
+				leading = false;
 
-			links.push(url);
+				links.push(url);
 
-			facets.push({
-				index: {
-					byteStart: start,
-					byteEnd: length,
-				},
-				features: [
-					{
-						$type: 'app.bsky.richtext.facet#link',
-						uri: url,
+				facets.push({
+					index: {
+						byteStart: start,
+						byteEnd: length,
 					},
-				],
-			});
+					features: [
+						{
+							$type: 'app.bsky.richtext.facet#link',
+							uri: url,
+						},
+					],
+				});
+			}
 		}
 	};
 

1296b50e64b26dd60255842acf8ec5bec2cc4543
Mary <pineapplecreamcheese@skiff.com>
2023-10-28 20:37:35 +0700

    refactor: use ProfileItem for mutes

--- src/routes/u.$uid.you.moderation.muted._index/route.tsx
+++ src/routes/u.$uid.you.moderation.muted._index/route.tsx
@@ -2,7 +2,6 @@ import { For, Match, Show, Switch, createEffect } from 'solid-js';
 
 import type { DID } from '@externdefs/bluesky-client/atp-schema';
 import { createQuery } from '@intrnl/sq';
-import { useNavigate } from '@solidjs/router';
 
 import type { SignalizedProfile } from '~/api/cache/profiles.ts';
 import type { ProfilesListPage } from '~/api/models/profiles-list.ts';
@@ -16,11 +15,14 @@ import {
 import { getCollectionCursor } from '~/api/utils.ts';
 
 import { openModal } from '~/globals/modals.tsx';
-import { generatePath, useParams } from '~/router.ts';
+import { useParams } from '~/router.ts';
 import { Title } from '~/utils/meta.tsx';
-import { INTERACTION_TAGS, isElementAltClicked, isElementClicked } from '~/utils/misc.ts';
 
 import MuteConfirmDialog from '~/components/dialogs/MuteConfirmDialog.tsx';
+import ProfileItem, {
+	createProfileItemKey,
+	type ProfileItemAccessory,
+} from '~/components/lists/ProfileItem.tsx';
 import CircularProgress from '~/components/CircularProgress.tsx';
 import VirtualContainer from '~/components/VirtualContainer.tsx';
 import button from '~/styles/primitives/button.ts';
@@ -28,9 +30,31 @@ import button from '~/styles/primitives/button.ts';
 import VolumeOffIcon from '~/icons/baseline-volume-off.tsx';
 import VolumeUpIcon from '~/icons/baseline-volume-up.tsx';
 
+const MuteAccessory: ProfileItemAccessory = {
+	key: 'm',
+	render: (profile, uid) => {
+		const isMuted = () => profile.viewer.muted.value;
+
+		return (
+			<button
+				onClick={() => {
+					openModal(() => <MuteConfirmDialog uid={uid} profile={profile} />);
+				}}
+				class="group flex h-9 w-9 items-center justify-center rounded-full border"
+				classList={{
+					'is-active border-red-900 hover:bg-red-600/20': isMuted(),
+					'border-input hover:bg-secondary': !isMuted(),
+				}}
+			>
+				<VolumeUpIcon class="text-xl text-accent group-[.is-active]:hidden" />
+				<VolumeOffIcon class="hidden text-xl text-red-500 group-[.is-active]:block" />
+			</button>
+		);
+	},
+};
+
 const AuthenticatedMutedUsersModerationPage = () => {
 	const params = useParams('/u/:uid/you/moderation/muted');
-	const navigate = useNavigate();
 
 	const uid = () => params.uid as DID;
 
@@ -115,72 +139,9 @@ const AuthenticatedMutedUsersModerationPage = () => {
 			<For each={mutes()?.pages}>
 				{(page) => {
 					return page.profiles.map((profile) => {
-						const isMuted = () => profile.viewer.muted.value;
-
-						const handleClick = (ev: MouseEvent | KeyboardEvent) => {
-							if (!isElementClicked(ev, INTERACTION_TAGS)) {
-								return;
-							}
-
-							const path = generatePath('/u/:uid/profile/:actor', {
-								uid: uid(),
-								actor: profile.did,
-							});
-
-							if (isElementAltClicked(ev)) {
-								open(path, '_blank');
-							} else {
-								navigate(path);
-							}
-						};
-
 						return (
-							<VirtualContainer id={/* @once */ `profile/${profile.did}/m`} estimateHeight={88}>
-								<div
-									onClick={handleClick}
-									onAuxClick={handleClick}
-									onKeyDown={handleClick}
-									role="button"
-									tabindex={0}
-									class="flex gap-3 px-4 py-3 hover:bg-hinted"
-								>
-									<div class="h-12 w-12 shrink-0 overflow-hidden rounded-full bg-muted-fg">
-										<Show when={profile.avatar.value}>
-											{(avatar) => <img src={avatar()} class="h-full w-full" />}
-										</Show>
-									</div>
-
-									<div class="flex min-w-0 grow flex-col gap-1">
-										<div class="flex items-center justify-between gap-3">
-											<div class="flex flex-col text-sm">
-												<span dir="auto" class="line-clamp-1 break-all font-bold">
-													{profile.displayName.value || profile.handle.value}
-												</span>
-												<span class="line-clamp-1 break-all text-muted-fg">@{profile.handle.value}</span>
-											</div>
-
-											<div>
-												<button
-													onClick={() => {
-														openModal(() => <MuteConfirmDialog uid={uid()} profile={profile} />);
-													}}
-													class="group flex h-9 w-9 items-center justify-center rounded-full border"
-													classList={{
-														'is-active border-red-900 hover:bg-red-600/20': isMuted(),
-														'border-input hover:bg-secondary': !isMuted(),
-													}}
-												>
-													<VolumeUpIcon class="text-xl text-accent group-[.is-active]:hidden" />
-													<VolumeOffIcon class="hidden text-xl text-red-500 group-[.is-active]:block" />
-												</button>
-											</div>
-										</div>
-
-										<Show when={profile.description.value}>
-											<div class="line-clamp-3 break-words text-sm">{profile.$renderedDescription()}</div>
-										</Show>
-									</div>
-								</div>
+							<VirtualContainer id={createProfileItemKey(profile)} estimateHeight={88}>
+								<ProfileItem uid={uid()} profile={profile} aside={MuteAccessory} />
 							</VirtualContainer>
 						);
 					});

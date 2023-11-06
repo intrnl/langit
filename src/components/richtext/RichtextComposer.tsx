f88ef0c631ba6fd518b84704f3733d5f28ac9fac
Mary <pineapplecreamcheese@skiff.com>
2023-11-06 18:54:20 +0700

    fix: reintroduce tracking on value

--- src/components/richtext/RichtextComposer.tsx
+++ src/components/richtext/RichtextComposer.tsx
@@ -8,7 +8,6 @@ import {
 	createResource,
 	createSignal,
 	onMount,
-	untrack,
 } from 'solid-js';
 import type { JSX } from 'solid-js/jsx-runtime';
 
@@ -98,12 +97,12 @@ const RichtextComposer = (props: RichtextComposerProps) => {
 
 	const candidateMatch = createMemo(() => {
 		const $cursor = inputCursor();
-		const $val = untrack(() => props.value);
 
 		if ($cursor == null) {
 			return '';
 		}
 
+		const $val = props.value;
 		return $val.length === $cursor ? $val : $val.slice(0, $cursor);
 	});
 

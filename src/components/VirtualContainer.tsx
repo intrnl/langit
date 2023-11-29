2ea0964b93e8f427e6b807c34dde2f59a3faf13d
Mary <pineapplecreamcheese@skiff.com>
2023-11-29 10:07:12 +0700

    Revert "refactor: always schedule idle task"
    This reverts commit 7e7743e70a7e8219cba6259eae1829f221721d27.

--- src/components/VirtualContainer.tsx
+++ src/components/VirtualContainer.tsx
@@ -39,15 +39,18 @@ const VirtualContainer = (props: VirtualContainerProps) => {
 
 		entry = nextEntry;
 
-		scheduleIdleTask(() => {
-			// Bail out if it's no longer entry.
-			if (entry !== nextEntry) {
-				return;
-			}
+		if (!prev && next) {
+			// Hidden -> Visible
+			setIntersecting(next);
+		} else if (prev && !next) {
+			// Visible -> Hidden
 
-			if (!prev && next) {
-				setIntersecting(next);
-			} else if (prev && !next) {
+			scheduleIdleTask(() => {
+				// Bail out if it's no longer us.
+				if (entry !== nextEntry) {
+					return;
+				}
+
 				const nextHeight = getRectFromEntry(nextEntry!).height;
 				const truncatedHeight = Math.trunc(nextHeight * 100) / 100;
 
@@ -61,8 +64,8 @@ const VirtualContainer = (props: VirtualContainerProps) => {
 				} else {
 					setIntersecting(next);
 				}
-			}
-		});
+			});
+		}
 	};
 
 	const measure = (node: HTMLElement) => scrollObserver.observe(node);

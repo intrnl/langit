b46ae7ff59f3ec912b64d3934bd96dde45c4c0f4
Mary <pineapplecreamcheese@skiff.com>
2023-11-29 10:01:29 +0700

    refactor: use requestAnimationFrame on visible

--- src/components/VirtualContainer.tsx
+++ src/components/VirtualContainer.tsx
@@ -39,15 +39,24 @@ const VirtualContainer = (props: VirtualContainerProps) => {
 
 		entry = nextEntry;
 
-		scheduleIdleTask(() => {
-			// Bail out if it's no longer entry.
-			if (entry !== nextEntry) {
-				return;
-			}
+		if (!prev && next) {
+			// Hidden -> Visible
+			requestAnimationFrame(() => {
+				// Bail out if it's no longer us.
+				if (entry !== nextEntry) {
+					return;
+				}
 
-			if (!prev && next) {
 				setIntersecting(next);
-			} else if (prev && !next) {
+			});
+		} else if (prev && !next) {
+			// Visible -> Hidden
+			scheduleIdleTask(() => {
+				// Bail out if it's no longer us.
+				if (entry !== nextEntry) {
+					return;
+				}
+
 				const nextHeight = getRectFromEntry(nextEntry!).height;
 				const truncatedHeight = Math.trunc(nextHeight * 100) / 100;
 
@@ -61,8 +70,8 @@ const VirtualContainer = (props: VirtualContainerProps) => {
 				} else {
 					setIntersecting(next);
 				}
-			}
-		});
+			});
+		}
 	};
 
 	const measure = (node: HTMLElement) => scrollObserver.observe(node);
